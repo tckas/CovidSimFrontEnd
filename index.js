a9a7f7c972841ca706a9447dece0d0f5eabbbcd8
Tiago Castilho <tcastilhowork@gmail.com>
2022-07-04 16:19:17 +0100

    heroku

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
+
+
+
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8080;
+
 const path = require('path');
 app.use(express.static(__dirname + "/public"));
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, '/html/index.html'));
 });
 
-app.listen(port);
-console.log('Server started at http://localhost:' + port);
